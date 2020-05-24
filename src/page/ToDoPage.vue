bb996a25a2bb8d59afe2f7166dba450f2f35876b
hgcassiopeia <ph.thitikarn_st@tni.ac.th>
2020-05-24 14:44:56 +0700

    mockup task renderer

--- src/page/ToDoPage.vue
+++ src/page/ToDoPage.vue
@@ -1,6 +1,25 @@
 <template>
     <div class="container">
-        Hello
+        <div class="task-zone">
+            <div class="drop-zone">
+                <h1>To-Do</h1>
+                <div class="drag-el" >
+                    Item A
+                </div>
+            </div>
+            <div class="drop-zone">
+                <h1>Doing</h1>
+                <div class="drag-el" >
+                    Item B
+                </div>
+            </div>
+            <div class="drop-zone">
+                <h1>Done</h1>
+                <div class="drag-el" >
+                    Item C
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -11,5 +30,26 @@ export default {
 </script>
 
 <style scoped>
-
+.container{
+    margin: 30px 0;
+}
+.task-zone{
+    display: flex;
+    justify-content: space-around;
+}
+.drop-zone{
+    border: 1px solid black;
+    border-radius: 10px;
+    width: 250px;
+    height: 400px;
+    padding: 10px 0;
+}
+.drag-el{
+    border: 1px solid black;
+    width: 200px;
+    height: 40px;
+    border-radius: 10px;
+    margin: 5px auto;
+    padding-top: 15px;
+}
 </style>
