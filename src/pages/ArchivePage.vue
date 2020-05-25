126a018db93d69e4f0d4e44b62c92afdccef0e9d
hgcassiopeia <ph.thitikarn_st@tni.ac.th>
2020-05-25 09:12:18 +0700

    add archive page

---
+++ src/pages/ArchivePage.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <CardList :archives="archives" />
+    </div>
+</template>
+
+<script>
+import CardList from '../components/CardList'
+export default {
+    name: 'ArchivePage',
+    components: { CardList },
+    data(){
+        return {
+            archives:[
+                {
+                    date: "22-05-2020",
+                    time: "19:00",
+                    tasks: [
+                        { id: 1, title: "Item A", status: "done" },
+                        { id: 2, title: "Item B", status: "done" },
+                    ]
+                },
+                {
+                    date: "24-05-2020",
+                    time: "19:00",
+                    tasks: [
+                        { id: 1, title: "Item A", status: "done" },
+                        { id: 2, title: "Item B", status: "done" },
+                    ]
+                }
+            ]
+        }
+    }
+}
+</script>
